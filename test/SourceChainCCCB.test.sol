c8afe7f3af4eec963f147b46ca1ddd5c6690dd09
mvargas33 <mvargas33@protonmail.com>
2023-12-02 03:17:08 -0300

    :fire: Delete unused files and start test

---
+++ test/SourceChainCCCB.test.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {SourceChainCCCB} from "../src/SourceChainCCCB.sol";
+
+contract SourceChainCCCBTest is Test {
+    SourceChainCCCB public sourceContract;
+
+    function setUp() public {
+        // SourceChainCCCB = new SourceChainCCCB();
+    }
+}
