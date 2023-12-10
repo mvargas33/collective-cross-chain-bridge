412d20fdd4c6eb6554613ef5655e25b6f29b28c3
mvargas33 <mvargas33@protonmail.com>
2023-12-10 17:37:17 -0300

    :hammer: Fix solidity versions

--- test/SourceChainCCCB.test.sol
+++ test/SourceChainCCCB.test.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache License 2.0
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Test, console} from "forge-std/Test.sol";
